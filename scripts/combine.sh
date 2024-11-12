32563a082fd1ef78e257143988818b036f10ea32
Siak Hooi <siakhooi@gmail.com>
2024-11-12 12:02:00 +0000

    bash script with google style

--- scripts/combine.sh
+++ scripts/combine.sh
@@ -3,18 +3,23 @@
 echo "menu:"
 
 cd src || exit 1
-ls -d * | sort -n | while read -r group1; do
-  name1=$(echo $group1 | sed 's/[^-]*-//')
+find . -mindepth 1 -maxdepth 1 -type d | sort -n | while read -r dir1; do
+  group1=$(basename "$dir1")
+
+  # shellcheck disable=SC2001
+  name1="$(echo "$group1" | sed 's/[^-]*-//')"
+
   echo "  - name: $name1"
   echo "    menu:"
   cd "$group1" || exit
 
-  ls -d *.yaml | sort -n | while read -r group2; do
-    name2=$(echo $group2 | sed 's/[^-]*-//' | sed 's/\.yaml$//')
+  find . -mindepth 1 -maxdepth 1 -type f -name '*.yaml' -exec basename {} \; | sort -n | while read -r group2; do
+    name2=$(echo "$group2" | sed 's/[^-]*-//' | sed 's/\.yaml$//')
     echo "      - name: $name2"
     echo "        menu:"
-    cat "$group2" | prefix '          '
+    prefix '          ' "$group2"
   done
+
   cd .. || exit 1
 done
 
